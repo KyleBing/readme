da5d4ec2c412455019619297f11cfaf703614d40
KyleBing(MBP14) <kylebing@163.com>
2025-12-04 10:45:27 +0800

    更新日记说明

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 import {defineConfig} from 'vite'
 import zipPack from "vite-plugin-zip-pack" // make dist.zip file
 
+// 因为直接部署到 github 中，所以不需要打包成 zip 了
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-/*        zipPack({
-            inDir: 'dist',
-            outDir: 'archive',
-            outFileName: `readme.zip`,
-            pathPrefix: ''
-        })*/
+        // zipPack({
+        //     inDir: 'dist',
+        //     outDir: 'archive',
+        //     outFileName: `readme.zip`,
+        //     pathPrefix: ''
+        // })
     ],
 })
